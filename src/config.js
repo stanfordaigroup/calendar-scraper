49b1679aab20a205d885660b4dc0b06c95a4b6e3
Andrew Lee <andrewsoohwanlee@gmail.com>
2017-11-05 01:11:46 -0700

    Initial commit. Basic functionality setup.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+const GitHubToken = require('../.credentials/github_token').token;
+const GoogleCalendar = require('../.credentials/google_calendar');
+
 const repoOptions = {
   owner: 'stanfordaigroup',
   repo: 'stanfordai.group',
@@ -6,4 +9,6 @@ const repoOptions = {
 
 module.exports = {
   repoOptions,
+  token: GitHubToken,
+  googleCalendar: GoogleCalendar,
 };
--- src/main.js
+++ src/main.js
@@ -1,35 +1,122 @@
 const CONFIG = require("./config");
 const GitHubApi = require('github');
-const base64 = require('base-64');
-const GitHubToken = require('../.credentials/github_token').token;
+const base64 = require('js-base64').Base64;
+const CalendarAPI = require('node-google-calendar');
+
+const cal = new CalendarAPI(CONFIG.googleCalendar);
 
 const github = new GitHubApi({});
 
-github.authenticate({
-  type: 'token',
-  token: GitHubToken,
-});
+async function init() {
+  let hasChanged = false;
+  let newCalendarData = null;
+  let fileSHA = null;
 
-let fileSHA = null;
-
-const timestamp = new Date();
-
-github.repos.getContent({
-  ...CONFIG.repoOptions,
-}).then(data => {
-  // Fetch the latest SHA of the requested file
-  fileSHA = data.data.sha;
-
-  github.repos.updateFile({
-    ...CONFIG.repoOptions,
-    message: 'Automated calendar update: ' + timestamp,
-    content: base64.encode(timestamp),
-    sha: fileSHA,
-  }).then(data => {
-    console.log(data);
-  }).catch(error => {
-    console.error(error)
+  // Setup GitHub authentication
+  github.authenticate({
+    type: 'token',
+    token: CONFIG.token,
   });
-}).catch(error => {
-  console.error(error);
-});
+
+  // Fetch the current JSON file in repo
+  const curGitHubData = await fetchGitHubFile();
+  const curJSONContent = JSON.stringify(base64.decode(curGitHubData.content));
+
+  // Fetch upcoming events from GCal and generate a JSON file
+  const curCalData = await fetchCalendarData();
+
+  const parsedCalData = {
+    data: curCalData.map(event => {
+      return {
+        summary: event.summary,
+        description: event.description,
+        location: event.location,
+        start: event.start,
+        end: event.end,
+        updated: event.updated,
+      };
+    }),
+  }
+  
+  newCalendarData = JSON.stringify(parsedCalData);
+
+  // Diff new and old JSON files
+  if (curJSONContent !== newCalendarData) {
+    hasChanged = true;
+  }
+  
+
+  // Update file (commit to repo) if there has been changes
+  if (hasChanged && newCalendarData) {
+    updateCalendarFile(curGitHubData, newCalendarData);
+  } else {
+    console.log('🚫 — No changes made.');
+  }
+}
+
+/**
+ * 
+ * 
+ * @param {any} data — JSON data to replace contents of calendar file
+ */
+function updateCalendarFile(curGitHubData, newCalendarData) {
+  github.repos.getContent({
+    ...CONFIG.repoOptions,
+  }).then(data => {
+    // Fetch the latest SHA of the requested file
+    const fileSHA = curGitHubData.sha;
+    const timestamp = new Date();
+  
+    // Now update the file
+    github.repos.updateFile({
+      ...CONFIG.repoOptions,
+      message: 'Automated calendar update: ' + timestamp,
+      content: base64.encode(newCalendarData),
+      sha: fileSHA,
+    }).then(data => {
+      console.log('Success! 🎉');
+      console.log('Updated file "' + CONFIG.repoOptions.path + '" on ' + timestamp + '.');
+    }).catch(error => {
+      console.error('Error updating file: ', CONFIG.repoOptions.path, error);
+    });
+  }).catch(error => {
+    console.error('Error fetching file content for: ', CONFIG.repoOptions.path, error);
+  }); 
+}
+
+/**
+ * Fetches the latest commit info for the given repo.
+ * 
+ * @returns Object of repo info.
+ */
+async function fetchGitHubFile() {
+  let response;
+
+  try {
+    response = await github.repos.getContent(CONFIG.repoOptions);
+    response = response.data;
+  } catch (error) {
+    console.error('Error fetching file content for: ', CONFIG.repoOptions.path, error);
+  }
+
+  return response;
+}
+
+async function fetchCalendarData() {
+  let response;
+
+  const params = {
+    showHidden: true,
+    timeMin: new Date(Date.now()).toISOString(),
+  };
+
+  try {
+    response = await cal.Events.list(CONFIG.googleCalendar.calendarId.saig, params);
+  } catch (error) {
+    console.error('Error fetching calendar data for: ', error);
+  }
+
+  return response;
+}
+
+init();
